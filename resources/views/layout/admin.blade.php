56bfcad4639930b693959f2d805eef839f892b45
MasThor <thaariqprs97@gmail.com>
2024-04-06 05:45:53 +0700

    fix issue

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -38,7 +38,7 @@
                     <ul class="menu">
                         <li class="sidebar-title">Menu</li>
 
-                        <li class="sidebar-item  ">
+                        {{-- <li class="sidebar-item  ">
                             <a href="dashboard.html" class='sidebar-link'>
                                 <i class="bi bi-grid-fill"></i>
                                 <span>Dashboard</span>
@@ -81,11 +81,11 @@
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a href="{{ url("verifikator-library.html") }}" class="sidebar-link">
+                            <a href="{{ url("verifikator-desk.html") }}" class="sidebar-link">
                                 <i class="bi bi-book"></i>
-                                <span>Verifikator Library</span>
+                                <span>Verifikator Desk</span>
                             </a>
-                        </li>
+                        </li> --}}
                     </ul>
                 </div>
                 <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
